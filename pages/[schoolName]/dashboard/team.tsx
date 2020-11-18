9300aa4097fc76f64845e9f18d0b1bb8e771ef95
wdwcs <arminit3@gmail.com>
2020-11-18 16:26:28 +0100

    team page

---
+++ pages/[schoolName]/dashboard/team.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Profile } from 'components';
+
+const Team = () => {
+	return (
+		<div className="flex flex-col">
+			<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+				<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+					<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+						<table className="min-w-full divide-y divide-gray-200">
+							<thead>
+								<tr>
+									<th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+										Name
+									</th>
+									<th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+										Title
+									</th>
+									<th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+										Status
+									</th>
+									<th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+										Role
+									</th>
+									<th className="px-6 py-3 bg-gray-50" />
+								</tr>
+							</thead>
+							<tbody className="bg-white divide-y divide-gray-200">
+								<Profile />
+								<Profile />
+								<Profile />
+								<Profile />
+								<Profile />
+								<Profile />
+								<Profile />
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Team;
