f3816deaefae358e22aaf894d03ba125448b3147
wdwcs <arminit3@gmail.com>
2020-11-18 15:24:07 +0100

    config plop

---
+++ plopfile.ts
@@ -0,0 +1,30 @@
+// @ts-ignore
+module.exports = plop => {
+	plop.setGenerator('component', {
+		description: 'Create a reusable component',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What is your component name ?'
+			}
+		],
+		actions: [
+			{
+				type: 'add',
+				path: 'components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+				templateFile: 'templates/Component.tsx.hbs'
+			},
+			{
+				type: 'add',
+				path: 'components/{{pascalCase name}}/{{pascalCase name}}.module.scss'
+			},
+			{
+				type: 'append',
+				path: 'components/index.ts',
+				pattern: `/* INJECT_EXPORT */`,
+				template: `export { default as {{pascalCase name}} } from './{{pascalCase name}}/{{pascalCase name}}';`
+			}
+		]
+	});
+};
