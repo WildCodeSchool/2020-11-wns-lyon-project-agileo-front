d1ce483731dbb637388280b6ed5b27751495e523
wdwcs <arminit3@gmail.com>
2020-11-18 16:27:20 +0100

    update

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+/* INJECT_EXPORT */
+export { default as HeaderMyProfile } from './Globals/HeaderMyProfile';
+export { default as Profile } from './Globals/Profile';
+export { default as Header } from './Globals/Header';
+export { default as HeaderDashboard } from './Globals/HeaderDashboard';
+export { default as Slide } from './Globals/Slide';
+export { default as Switch } from './Globals/Switch';
