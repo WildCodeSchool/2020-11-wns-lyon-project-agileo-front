e0c5ba91be220f871e4f316094042c9e2197c177
wdwcs <arminit3@gmail.com>
2020-11-18 16:24:44 +0100

    add meet

---
+++ pages/[schoolName]/dashboard/meet.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Meet = () => {
+	const classeID = 'Classe-07894512';
+	return <iframe src={`https://meet.jit.si/${classeID}`} style={{ height: 720, width: '100%' }} />;
+};
+
+export default Meet;
