153f5f709332eda2cd5e551b30e14e4710626f4b
wdwcs <arminit3@gmail.com>
2020-11-18 16:26:18 +0100

    profile component

---
+++ components/Globals/Profile.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<tr>
+			<td className="px-6 py-4 whitespace-no-wrap">
+				<div className="flex items-center">
+					<div className="flex-shrink-0 h-10 w-10">
+						<img
+							className="h-10 w-10 rounded-full"
+							src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+							alt=""
+						/>
+					</div>
+					<div className="ml-4">
+						<div className="text-sm leading-5 font-medium text-gray-900">Jane Cooper</div>
+						<div className="text-sm leading-5 text-gray-500">jane.cooper@example.com</div>
+					</div>
+				</div>
+			</td>
+			<td className="px-6 py-4 whitespace-no-wrap">
+				<div className="text-sm leading-5 text-gray-900">Regional Paradigm Technician</div>
+				<div className="text-sm leading-5 text-gray-500">Optimization</div>
+			</td>
+			<td className="px-6 py-4 whitespace-no-wrap">
+				<span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+					Active
+				</span>
+			</td>
+			<td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">Student</td>
+			<td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+				<a href="#" className="text-indigo-600 hover:text-indigo-900">
+					Edit
+				</a>
+			</td>
+		</tr>
+	);
+};
+
+export default Profile;
