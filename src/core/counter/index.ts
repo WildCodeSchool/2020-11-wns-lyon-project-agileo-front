00ac2b5de4bfcca2130257e9274519514b17e476
Armin <arminit3@gmail.com>
2020-12-13 22:25:06 +0100

    add an example of counter with redux

---
+++ src/core/counter/index.ts
@@ -0,0 +1,4 @@
+import reducer from './reducers'
+import * as counterActions from './actions'
+export { counterActions }
+export default reducer
---
+++ src/core/index.ts
@@ -0,0 +1,3 @@
+/* INJECT_EXPORT */
+export { default as counter } from './counter'
+export { default as clock } from './clock'
