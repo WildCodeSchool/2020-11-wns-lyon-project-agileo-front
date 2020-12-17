1368f6e9bf2e6f62715e7d452d7386a3269dc6d3
Armin <arminit3@gmail.com>
2020-12-17 11:57:35 +0100

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const Header = () => {
-  const links = [
-    { href: '/', label: 'Home' },
-    { href: '/', label: 'Features' },
-    { href: '/', label: 'Pricing' },
-    { href: '/', label: 'Contact Us' },
-    { href: '/example-with-redux', label: 'Redux' },
-  ]
-
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10 relative bg-white">
       <div className="lg:w-0 lg:flex-1">
         <Link href="/">
           <a className="flex">
-            <Image className="h-10 w-auto sm:h-12" src="/images/logo.svg" alt="Workflow" width={50} height={50} />
+            <Image className="h-10 w-auto sm:h-12" src="/images/logo.svg" alt="Workflow" width={60} height={60} />
           </a>
         </Link>
       </div>
@@ -37,19 +29,17 @@ const Header = () => {
         </button>
       </div>
       <nav className="hidden md:flex space-x-10">
-        {links.map(({ href, label }, key) => (
-          <Link key={key} href={href}>
-            <a className="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
-              {label}
-            </a>
-          </Link>
-        ))}
+        <Link href="/">
+          <a className="flex">
+            <Image className="h-10 w-auto sm:h-12" src="/images/agileo.svg" alt="Workflow" width={120} height={60} />
+          </a>
+        </Link>
       </nav>
       <div className="hidden md:flex items-center justify-end space-x-8 md:flex-1 lg:w-0">
         <span className="inline-flex rounded-md shadow-sm">
           <Link href="/login">
-            <a className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
-              Sign in
+            <a className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-300 focus:outline-none focus:border-yellow-500 focus:shadow-outline-yellow active:bg-yellow-500 transition ease-in-out duration-150">
+              Login
             </a>
           </Link>
         </span>
