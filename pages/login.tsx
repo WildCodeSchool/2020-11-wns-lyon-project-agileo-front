ab7b8e1e60ae56f3231751a99c051491263d1c2a
Armin <arminit3@gmail.com>
2020-12-17 11:59:08 +0100

    implementation of authentication login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
+
+const AUTHENTICATE = gql`
+  mutation authenticate($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      item {
+        id
+      }
+    }
+  }
+`
+
+const AUTHENTICATED_USER = gql`
+  query authenticatedUser {
+    authenticatedUser {
+      id
+    }
+  }
+`
 
 const SignIn = () => {
+  const [email, setEmail] = useState('admin')
+  const [password, setPassword] = useState('adminadmin')
+  const router = useRouter()
+
+  const { data: { authenticatedUser } = {}, loading, error } = useQuery(AUTHENTICATED_USER)
+  const [authenticate, { loading: signingIn, error: signinError }] = useMutation(AUTHENTICATE, {
+    refetchQueries: ['authenticatedUser'],
+  })
+
+  const handleClick = async (e: any) => {
+    e.preventDefault()
+    await authenticate({
+      variables: {
+        email,
+        password,
+      },
+    })
+  }
+
+  if (authenticatedUser) {
+    router.push('/schoolName/dashboard')
+  }
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full">
@@ -9,55 +51,68 @@ const SignIn = () => {
           <img className="mx-auto h-16 w-auto" src="/images/logo.svg" alt="Workflow" />
           <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">Sign in to your account</h2>
         </div>
-        <form className="mt-8" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm">
-            <div>
-              <input
-                aria-label="Email address"
-                name="email"
-                type="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Email address"
-              />
-            </div>
-            <div className="-mt-px">
-              <input
-                aria-label="Password"
-                name="password"
-                type="password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
-                placeholder="Password"
-              />
-            </div>
-          </div>
 
-          <div className="mt-6 flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember_me"
-                type="checkbox"
-                className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-              />
-              <label htmlFor="remember_me" className="ml-2 block text-sm leading-5 text-gray-900">
-                Remember me
-              </label>
+        {loading || signingIn ? (
+          <p>loading...</p>
+        ) : error || signinError ? (
+          <p>Error!</p>
+        ) : (
+          <form className="mt-8" action="#" onSubmit={handleClick}>
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm">
+              <div>
+                <input
+                  aria-label="Email address"
+                  name="email"
+                  type="name"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value)
+                  }}
+                />
+              </div>
+              <div className="-mt-px">
+                <input
+                  aria-label="Password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => {
+                    setPassword(event.target.value)
+                  }}
+                />
+              </div>
             </div>
 
-            <div className="text-sm leading-5">
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
-              >
-                Forgot your password?
-              </a>
-            </div>
-          </div>
+            <div className="mt-6 flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember_me"
+                  type="checkbox"
+                  className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                />
+                <label htmlFor="remember_me" className="ml-2 block text-sm leading-5 text-gray-900">
+                  Remember me
+                </label>
+              </div>
 
-          <div className="mt-6">
-            <Link href="/schoolName/dashboard">
+              <div className="text-sm leading-5">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-6">
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
@@ -77,9 +132,9 @@ const SignIn = () => {
                 </span>
                 Sign in
               </button>
-            </Link>
-          </div>
-        </form>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   )
