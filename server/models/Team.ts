b6bc239e38a547dfe838b246a058447556680654
wdwcs <arminit3@gmail.com>
2021-02-02 19:08:21 +0100

    add fields for models

--- server/models/Team.ts
+++ server/models/Team.ts
@@ -1,10 +1,9 @@
-import { Text } from '@keystonejs/fields'
 import { App } from '../controllers/App'
+import { Text, Relationship } from '@keystonejs/fields'
 
 App.keystone.createList('Team', {
   fields: {
     name: { type: Text, isRequired: true },
+    company: { type: Relationship, ref: 'Company', isRequired: true },
   },
-  plugins: [],
-  access: {},
 })
