a3239be02c338a229b1183190afb5885b2c275f2
Anaïs Rouvière <anais.rouviere@kobalt.fr>
2021-02-03 10:56:00 +0100

    HomePage : Ajout de fonts + favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import 'tailwindcss/tailwind.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import React from 'react'
@@ -6,6 +7,7 @@ import { Provider } from 'react-redux'
 import { useStore } from 'config/store'
 import { useApollo } from 'config/apollo'
 import HeaderDashboard from 'components/HeaderDashboard'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   const store = useStore(pageProps.initialReduxState)
@@ -13,6 +15,14 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 
   return (
     <Provider store={store}>
+      <CssBaseline />
+      <Head>
+        <link rel="shortcut icon" type="image/svg" href="logo.svg" />
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="minimum{/**/}-scale=1, initial-scale=1, width=device-width" />
+        <title>Agileo - Une façon innovante d&apos;enseigner à distance</title>
+      </Head>
       <ApolloProvider client={apolloClient}>
         {router.query.schoolName ? (
           <>
