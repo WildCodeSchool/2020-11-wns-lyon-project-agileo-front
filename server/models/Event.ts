26c8f7070e6dcd08c865013877816262b50b0c6a
Sora952 <camillebueno952@gmail.com>
2021-02-04 20:59:00 +0100

    add calendar

--- server/models/Event.ts
+++ server/models/Event.ts
@@ -1,14 +1,16 @@
 import { App } from '../index'
-import { Text, Checkbox, Relationship } from '@keystonejs/fields'
+import { Text, Relationship } from '@keystonejs/fields'
 
 App.keystone.createList('Event', {
   fields: {
-    name: { type: Text, isRequired: true },
-    subject: { type: Text, isRequired: true },
-    start: { type: Text },
-    end: { type: Text },
-    allDay: { type: Checkbox },
+    title: { type: Text, isRequired: true },
+    notes: { type: Text },
+    startDate: { type: Text, isRequired: true },
+    endDate: { type: Text, isRequired: true },
+    rRule: { type: Text },
+    location: { type: Text, isRequired: true },
+    type: { type: Text, isRequired: true },
     class: { type: Relationship, ref: 'Class' },
-    user: { type: Relationship, ref: 'User', many: true },
+    user: { type: Relationship, ref: 'User' },
   },
 })
