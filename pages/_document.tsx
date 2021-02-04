533cda7def9748d2a2ccf78817372340cb4f8b1f
wdwcs <arminit3@gmail.com>
2021-02-04 13:42:40 +0100

    fix material ui error

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document from 'next/document'
+import React from 'react'
+import { ServerStyleSheets } from '@material-ui/core/styles'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      })
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    }
+  }
+}
