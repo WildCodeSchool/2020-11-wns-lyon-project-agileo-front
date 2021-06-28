829947ddd1362eab65d2c4a3730f85a01393b0aa
Sora952 <sora952c16b10@gmail.com>
2021-06-28 16:12:49 +0200

    add calendar camille

--- api/models/Event.ts
+++ api/models/Event.ts
@@ -2,14 +2,9 @@ import {App} from '../index'
 import {Relationship, Text} from '@keystonejs/fields'
 
 App.keystone.createList('Event', {
-    fields: {
-        title: {type: Text, isRequired: true},
-        notes: {type: Text},
-        startDate: {type: Text, isRequired: true},
-        endDate: {type: Text, isRequired: true},
-        rRule: {type: Text},
-        location: {type: Text, isRequired: true},
-        type: {type: Text, isRequired: true},
-        user: {type: Relationship, ref: 'User'},
-    },
+  fields: {
+    notes: { type: Text, isRequired: true },
+    startingDay: { type: Text, isRequired: true },
+    endingDay: { type: Text, isRequired: true },
+  },
 })
