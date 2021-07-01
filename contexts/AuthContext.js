c75a0aa4a2dd31f2661f019ed565fb39982a7bae
abdel <a.akroh@e-durable.ch>
2021-07-01 13:56:09 +0200

    WIP

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -3,14 +3,13 @@ import React, { createContext, useContext, useState } from "react";
 const authContext = createContext();
 
 export const ProvideAuth = (props) => {
-    const [token, setToken] = useState();
+    const [token, setToken] = useState(window.localstorage.getItem("auth_token"));
     const [currentUser, setcurrentUser] = useState({});
 
 
     const signin = (data) => {
         window.localStorage.setItem("auth_token", data.token);
         setToken(data.token);
-        console.log(data)
         setcurrentUser({
             firstName :data.item.firstName, 
             avatar :data.item.pictureUrl,
